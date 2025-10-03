990b197412d5496d1c426ad921bdea61fb70e1a0
Charvi143 <179401002+Charvi143@users.noreply.github.com>
2025-10-03 22:03:56 +0530

    .

--- backend/Models/db.js
+++ backend/Models/db.js
@@ -6,6 +6,7 @@ const mongo_url = process.env.MONGO_URL;
     try {
         await mongoose.connect(mongo_url);
         console.log('MongoDB Connected...');
+        
     } catch (err) {
         console.log("MongoDB Connection Error:", err)
     }
